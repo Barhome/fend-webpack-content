0d37cb71a048de135741fdf6da79e754bbacb532
barhome <barhome@github.com>
2021-02-09 12:45:44 +0300

    feat:Add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const webpack = require("webpack")
+const htmlWebPackPlugin =require('html-webpack-plugin')
 module.exports = {
     entry: './src/client/index.js',
     module: {
@@ -10,5 +11,12 @@ module.exports = {
             loader: "babel-loader"
                 }
         ]
-}
+    },
+    plugins:[
+        new htmlWebPackPlugin({
+        template:'./src/client/views/index.html',
+        filename:'./index.html',
+    })
+]
+
 }
